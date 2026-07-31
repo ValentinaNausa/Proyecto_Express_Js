174c135a76ec90e062a592790180b4bbd5769156
Valentina Nausa <Valentinanausa@gmail.com>
2026-07-31 11:30:53 -0500

    Trabajo En Clase 31/07/2026

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.get("/", (_, res) => {
-res.send("Aprendiendo express,ficha 3407181");
-});
-app.listen(port, () => {
-console.log( `Servidor en funcionamiento en el puerto: ${port}`);
-});
+//const express = require('express');
+import express from 'express'
+//leer el archivo .env
+import {configDotenv} from "dotenv"
+configDotenv()
+
+const app = express()
+const puerto = process.env.puerto
+
+app.get("/", function (req, res){
+res.send(` Hola aprendiendo express, con la ficha 3407181, ADSO en el SENA 31 Julio `);
+})
+//otro endpoint
+app.get("/otraruta",(req, res) => {
+//usando template string
+res.send(`<h1> Otro Ejemplo de ruta</h1>
+    <h2>End point con res.send</h2>`)
+})
+
+//otro endpoint
+app.get("/ruta2",(req, res) => {
+res.json({"nombre": "Valentina", "Apellido": "Nausa", "ficha": 3407181, "programa": "ADSO"})
+})
+
+//otro endpoint
+app.get("/ruta3/:aprendiz/:otrodato",(req, res) => {
+     const dato_aprendiz = req.params.aprendiz
+     const otro_dato = req.params.otrodato
+res.json({"nombre": dato_aprendiz, "Otro": otro_dato})
+})
+
+//otro endpoint
+app.get("/ruta4",(req, res) => {
+    //capturar el parametro de consulta query
+    const orden = req.query.orden || "sin ordenar"
+    const pagina = req.query.orden || 1
+res.send(`<h1>Listado Aprendices</h1>
+    <p>El listado esta en orden ${orden}</p>
+    <p>Pagina: ${pagina}</p>
+    `)
+})
+
+
+app.listen(puerto, function () {
+console.log( `SERVIDOR: http://localhost:${puerto}`)
+})
